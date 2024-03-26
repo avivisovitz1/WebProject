86a28ac26ef0395321eccb0658ec2e57a41612ba
avivisovitz1 <avivisovitz@cs.colman.ac.il>
2024-03-26 22:53:31 +0200

    add basic unit testing for student api

--- server/app.js
+++ server/app.js
@@ -1,22 +1,30 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
 const { studentRoute, postRoute } = require('./routes');
-const port = process.env.PORT || 3000;
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-mongoose.connect(process.env.DATABASE_URL);
-const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => { console.log('Connection Success'); });
+const init = () => {
+  const promise = new Promise((resolve, reject) => {
+    const db = mongoose.connection;
+    db.on('error', (error) => console.error(error));
+    db.once('open', () => { console.log('Connection Success'); });
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+    mongoose.connect(process.env.DATABASE_URL).then(() => {
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
 
-app.use('/student', studentRoute)
-app.use('/post', postRoute);
+      app.use('/student', studentRoute)
+      app.use('/post', postRoute);
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+      resolve(app);
+    });
+  });
+
+  return promise;
+}
+
+module.exports = init;
